01900b7f49fbb7aaae07cda105aacc1880252a70
Kristopher Overholt <koverholt@gmail.com>
2023-06-28 18:11:50 -0500

    Update app

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 # bot-comparison
 
-## Build and push the Docker image with Cloud Build
+## Build and deploy the app with Cloud Run
 
 ```
-gcloud builds submit --tag us-central1-docker.pkg.dev/gbot-test-062/chatbot-comparison/chatbot-comparison:1.3.0 .
-```
-
-## Deploy the app with Cloud Run
-
-```
-gcloud run deploy chatbot-comparison --image us-central1-docker.pkg.dev/gbot-test-062/chatbot-comparison/chatbot-comparison:1.3.0 --allow-unauthenticated --region us-central1 --port 5173
+gcloud run deploy chatbot-comparison . --source --allow-unauthenticated --region us-central1
 ```
 
 ## Access the app
--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,38 +1,90 @@
 <script lang="ts">
-	import { Input, Label, Helper } from 'flowbite-svelte';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Navbar, NavBrand } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > input').value = 'Hello';
+			}, 1000)
+		);
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > button')
+					.click();
+			}, 2000)
+		);
 
-		document.querySelectorAll('iframe').forEach( item =>
-		  setTimeout(function(){ item.contentWindow.document.body.querySelector("df-messenger").shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > input").value="Hello" }, 1000)
-		)
-		document.querySelectorAll('iframe').forEach( item =>
-		  setTimeout(function(){ item.contentWindow.document.body.querySelector("df-messenger").shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > button").click() }, 2000)
-		)
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > input').value =
+					'Does the Pixel 7 Pro support fast charging?';
+			}, 5000)
+		);
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > button')
+					.click();
+			}, 6000)
+		);
 
-    document.querySelectorAll('iframe').forEach( item =>
-		  setTimeout(function(){ item.contentWindow.document.body.querySelector("df-messenger").shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > input").value="Does the Pixel 7 Pro support fast charging?" }, 5000)
-		)
-    document.querySelectorAll('iframe').forEach( item =>
-		  setTimeout(function(){ item.contentWindow.document.body.querySelector("df-messenger").shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > button").click() }, 6000)
-		)
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > input').value =
+					'How does the Pixel Watch track my time sleeping?';
+			}, 15000)
+		);
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > button')
+					.click();
+			}, 16000)
+		);
 
-    document.querySelectorAll('iframe').forEach( item =>
-		  setTimeout(function(){ item.contentWindow.document.body.querySelector("df-messenger").shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > input").value="How does the Pixel Watch track my time sleeping?" }, 15000)
-		)
-    document.querySelectorAll('iframe').forEach( item =>
-		  setTimeout(function(){ item.contentWindow.document.body.querySelector("df-messenger").shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > button").click() }, 16000)
-		)
-
-    document.querySelectorAll('iframe').forEach( item =>
-		  setTimeout(function(){ item.contentWindow.document.body.querySelector("df-messenger").shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > input").value="How does the Nest Camera tell the difference between people, animals, vehicles, and packages?" }, 25000)
-		)
-    document.querySelectorAll('iframe').forEach( item =>
-		  setTimeout(function(){ item.contentWindow.document.body.querySelector("df-messenger").shadowRoot.querySelector("df-messenger-chat").shadowRoot.querySelector("df-messenger-user-input").shadowRoot.querySelector(".input-box-wrapper > button").click() }, 26000)
-		)
-
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > input').value =
+					'How does the Nest Camera tell the difference between people, animals, vehicles, and packages?';
+			}, 25000)
+		);
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > button')
+					.click();
+			}, 26000)
+		);
 	});
 </script>
 
@@ -44,22 +96,17 @@
 	</NavBrand>
 </Navbar>
 
-<!-- <form>
-	<Label for="first_name" class="mb-2">Input</Label>
-	<Input type="text" id="first_name" placeholder="Test" required />
-</form> -->
-
 <div class="flex">
 	<div class="w-1/4">
-		<iframe src="/bot1" height="600" class="mx-auto" />
+		<iframe src="/bot1" height="600" class="mx-auto" title="bot1" />
 	</div>
 	<div class="w-1/4">
-		<iframe src="/bot2" height="600" class="mx-auto" />
+		<iframe src="/bot2" height="600" class="mx-auto" title="bot2" />
 	</div>
 	<div class="w-1/4">
-		<iframe src="/bot3" height="600" class="mx-auto" />
+		<iframe src="/bot3" height="600" class="mx-auto" title="bot3" />
 	</div>
 	<div class="w-1/4">
-		<iframe src="/bot4" height="600" class="mx-auto" />
+		<iframe src="/bot4" height="600" class="mx-auto" title="bot4" />
 	</div>
 </div>
