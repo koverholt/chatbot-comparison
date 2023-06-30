5d23dc005613fd1854c735fa25217a68dd905fd1
Kristopher Overholt <koverholt@gmail.com>
2023-06-30 12:58:07 -0500

    Update

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,100 +1,59 @@
 <script lang="ts">
 	import { Navbar, NavBrand } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
+	import { Input, Label, Helper } from 'flowbite-svelte';
+	import { Button, ButtonGroup } from 'flowbite-svelte';
+
+	var utterance: string = 'Tell me about the Tensor G2 processor in the Pixel tablet';
+
+	function fillAndSend(utterance: string, time: number) {
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > input').value = utterance;
+			}, time * 1000)
+		);
+		document.querySelectorAll('iframe').forEach((item) =>
+			setTimeout(function () {
+				item.contentWindow.document.body
+					.querySelector('df-messenger')
+					.shadowRoot.querySelector('df-messenger-chat')
+					.shadowRoot.querySelector('df-messenger-user-input')
+					.shadowRoot.querySelector('.input-box-wrapper > button')
+					.click();
+			}, (time + 1) * 1000)
+		);
+	}
 
 	onMount(() => {
-		document.querySelectorAll('iframe').forEach((item) =>
-			setTimeout(function () {
-				item.contentWindow.document.body
-					.querySelector('df-messenger')
-					.shadowRoot.querySelector('df-messenger-chat')
-					.shadowRoot.querySelector('df-messenger-user-input')
-					.shadowRoot.querySelector('.input-box-wrapper > input').value = 'Hello';
-			}, 1000)
-		);
-		document.querySelectorAll('iframe').forEach((item) =>
-			setTimeout(function () {
-				item.contentWindow.document.body
-					.querySelector('df-messenger')
-					.shadowRoot.querySelector('df-messenger-chat')
-					.shadowRoot.querySelector('df-messenger-user-input')
-					.shadowRoot.querySelector('.input-box-wrapper > button')
-					.click();
-			}, 2000)
-		);
-
-		document.querySelectorAll('iframe').forEach((item) =>
-			setTimeout(function () {
-				item.contentWindow.document.body
-					.querySelector('df-messenger')
-					.shadowRoot.querySelector('df-messenger-chat')
-					.shadowRoot.querySelector('df-messenger-user-input')
-					.shadowRoot.querySelector('.input-box-wrapper > input').value =
-					'Does the Pixel 7 Pro support fast charging?';
-			}, 5000)
-		);
-		document.querySelectorAll('iframe').forEach((item) =>
-			setTimeout(function () {
-				item.contentWindow.document.body
-					.querySelector('df-messenger')
-					.shadowRoot.querySelector('df-messenger-chat')
-					.shadowRoot.querySelector('df-messenger-user-input')
-					.shadowRoot.querySelector('.input-box-wrapper > button')
-					.click();
-			}, 6000)
-		);
-
-		document.querySelectorAll('iframe').forEach((item) =>
-			setTimeout(function () {
-				item.contentWindow.document.body
-					.querySelector('df-messenger')
-					.shadowRoot.querySelector('df-messenger-chat')
-					.shadowRoot.querySelector('df-messenger-user-input')
-					.shadowRoot.querySelector('.input-box-wrapper > input').value =
-					'How does the Pixel Watch track my time sleeping?';
-			}, 15000)
-		);
-		document.querySelectorAll('iframe').forEach((item) =>
-			setTimeout(function () {
-				item.contentWindow.document.body
-					.querySelector('df-messenger')
-					.shadowRoot.querySelector('df-messenger-chat')
-					.shadowRoot.querySelector('df-messenger-user-input')
-					.shadowRoot.querySelector('.input-box-wrapper > button')
-					.click();
-			}, 16000)
-		);
-
-		document.querySelectorAll('iframe').forEach((item) =>
-			setTimeout(function () {
-				item.contentWindow.document.body
-					.querySelector('df-messenger')
-					.shadowRoot.querySelector('df-messenger-chat')
-					.shadowRoot.querySelector('df-messenger-user-input')
-					.shadowRoot.querySelector('.input-box-wrapper > input').value =
-					'How does the Nest Camera tell the difference between people, animals, vehicles, and packages?';
-			}, 25000)
-		);
-		document.querySelectorAll('iframe').forEach((item) =>
-			setTimeout(function () {
-				item.contentWindow.document.body
-					.querySelector('df-messenger')
-					.shadowRoot.querySelector('df-messenger-chat')
-					.shadowRoot.querySelector('df-messenger-user-input')
-					.shadowRoot.querySelector('.input-box-wrapper > button')
-					.click();
-			}, 26000)
-		);
+		fillAndSend('Hello', 1);
+		// fillAndSend('Does the Pixel 7 Pro support fast charging?', 5);
+		// fillAndSend('How does the Pixel Watch track sleep?', 15);
+		// fillAndSend('How does the Nest Camera tell the difference between people, animals, vehicles, and packages?',	25);
 	});
 </script>
 
-<Navbar let:hidden let:toggle>
+<Navbar let:hidden let:toggle class="bg-[#E8F0FE] p-6 mb-9">
 	<NavBrand href="/">
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-			Google Store Virtual Agent
+			Bot Comparison - Google Store Virtual Agent
 		</span>
 	</NavBrand>
 </Navbar>
+
+<div class="flex text-center">
+	<div class="w-0 grow">
+		<form>
+			<ButtonGroup class="w-1/2 mb-9">
+				<Input class="w-full" type="text" id="utterance" bind:value={utterance} required />
+				<Button color="blue" type="submit" on:click={fillAndSend(utterance, 0)}>Submit</Button>
+			</ButtonGroup>
+		</form>
+	</div>
+</div>
 
 <div class="flex">
 	<div class="w-1/4">
